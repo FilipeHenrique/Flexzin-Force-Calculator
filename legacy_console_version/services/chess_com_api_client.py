6c50081fba5cac807e7a6047529d0595146a1141
FilipeHenrique <filipevp1999@hotmail.com>
2025-11-23 13:28:55 -0300

    chore: move console app to legacy code folder

---
+++ legacy_console_version/services/chess_com_api_client.py
@@ -0,0 +1,36 @@
+import aiohttp
+import asyncio
+from datetime import datetime
+
+CHESS_COM_BASE_URL = "https://api.chess.com"
+
+class ChessComApiClient:
+    async def get_games_from_chess_com(self, session, player_nickname, year, month):
+        url = f"{CHESS_COM_BASE_URL}/pub/player/{player_nickname}/games/{year}/{month}"
+        headers = {"User-Agent": "flexzin-force/1.0"}
+        
+        async with session.get(url, headers=headers) as resp:
+            data = await resp.json()
+            return data.get("games", [])
+
+    async def get_player_games_from_last_six_months(self, player_nickname: str):
+        tasks = []
+        now = datetime.now()
+        year = now.year
+        month = now.month
+
+        months = []
+        for _ in range(6):
+            months.append((year, month))
+            month -= 1
+            if month == 0:
+                month = 12
+                year -= 1
+
+        async with aiohttp.ClientSession() as session:
+            for y, m in months:
+                tasks.append(
+                    self.get_games_from_chess_com(session, player_nickname, y, m)
+                )
+
+            return await asyncio.gather(*tasks)
---
+++ legacy_console_version/services/flexzin_force_calculator.py
@@ -0,0 +1,61 @@
+from math import sqrt
+from infrastructure.redis_repository import RedisRepository
+from services.chess_com_api_client import ChessComApiClient
+import json
+
+FLEXZIN_NICKNAME = "FIexPrime"
+TIME_CONTROLS = ["rapid", "blitz", "bullet"]
+HOUR_IN_SECONDS = 3600
+Z = 1.96
+
+class FlexzinForceCalculator:
+    def __init__(self, chess_com_api_client: ChessComApiClient, redis_repository: RedisRepository):
+        self.chess_com_api_client = chess_com_api_client    
+        self.redis_repository = redis_repository
+
+    async def get_flexzin_force_by_time_control(self, player_nickname: str):
+        cached_flexzin_force = await self.redis_repository.get(FLEXZIN_NICKNAME)
+        cached_player_force = await self.redis_repository.get(player_nickname)
+
+        if((cached_flexzin_force)):
+            flexzin_force_by_time_control = json.loads((cached_flexzin_force))
+        else:
+            flexzin_games_from_last_six_months = await self.chess_com_api_client.get_player_games_from_last_six_months(FLEXZIN_NICKNAME)
+            flexzin_force_by_time_control = self.calculate_player_force_by_time_control(flexzin_games_from_last_six_months, FLEXZIN_NICKNAME)
+            await self.redis_repository.set(FLEXZIN_NICKNAME, json.dumps(flexzin_force_by_time_control), expire= HOUR_IN_SECONDS)
+
+        if(cached_player_force):
+            player_force_by_time_control = json.loads(cached_player_force)
+        else:
+            player_games_from_last_six_months = await self.chess_com_api_client.get_player_games_from_last_six_months(player_nickname)
+            player_force_by_time_control = self.calculate_player_force_by_time_control(player_games_from_last_six_months, player_nickname)
+            await self.redis_repository.set(player_nickname, json.dumps(player_force_by_time_control), expire= HOUR_IN_SECONDS)         
+
+        flexzin_force_results_by_time_control = {}
+        for time_control, player_force in player_force_by_time_control.items():
+            if player_force is not None:
+                flexzin_force_results_by_time_control[time_control] = round(player_force_by_time_control[time_control]/flexzin_force_by_time_control[time_control],2)
+
+        await self.redis_repository.close()
+        return flexzin_force_results_by_time_control
+
+    def calculate_player_force_by_time_control(self, player_games_from_last_six_months, player_nickname):
+        player_ratings_by_time_control = {time_control: [] for time_control in TIME_CONTROLS}
+        for month in player_games_from_last_six_months: 
+            for game in month:     
+                player_rating = game["white"]["rating"] if player_nickname.lower() in game["white"]["username"].lower() else game["black"]["rating"]
+                if(game["time_class"] in TIME_CONTROLS) and game["rated"] is True:
+                    player_ratings_by_time_control[game["time_class"]].append(player_rating)
+
+        player_force_by_time_control = {}     
+        for time_control, ratings in player_ratings_by_time_control.items():
+            if not ratings:
+                player_force_by_time_control[time_control] = None
+                continue
+            average_rating = sum(ratings) / len(ratings)
+            square_sum = sum((r - average_rating) ** 2 for r in ratings)
+            standard_deviation = sqrt(square_sum / (len(ratings) - 1))
+            error_margin = Z * (standard_deviation / sqrt(len(ratings)))
+            player_force_by_time_control[time_control] = average_rating - error_margin
+
+        return player_force_by_time_control
